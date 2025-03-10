9c9466f7089b013599aaad92f80cdebf6e4de525
Gayasri72 <gayasripethum1@gmail.com>
2025-03-10 12:35:56 +0530

    downgrade tailwindcss to version 3 beacuse flowbite-react still not support for v4

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,5 +1,14 @@
+import { Navbar, TextInput } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return(
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>TRAVALIA</Link>
+      <form>
+        <TextInput/>
+      </form>
+    </Navbar>
+  );
 }
