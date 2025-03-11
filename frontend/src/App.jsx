aa5778fa80d17ad09b161df690162207ac6c4e01
Gayasri72 <gayasripethum1@gmail.com>
2025-03-11 09:26:12 +0530

    set up footer and move some folers in to src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import{BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from '../pages/Home'
-import Info from '../pages/Info'
-import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignUp'
-import ContactUs from '../pages/ContactUs'
-import Header from '../components/Header'
+import Home from './pages/Home'
+import Info from './pages/Info'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ContactUs from './pages/ContactUs'
+import Header from './components/Header'
+import Notfound from './pages/Notfound'
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -19,8 +21,10 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/contact" element={<ContactUs/>} />
+        <Route path="*" element={<Notfound/>} />
 
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
