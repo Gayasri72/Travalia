ec2051a7339669bb58b0c92f98bbbb685e55218f
KAVINDU <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2025-03-14 18:17:19 +0530

    add functionality to the sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import login from '../assets/userManagement/login.png';
 import icon1 from '../assets/userManagement/icon1.png';
 import icon2 from '../assets/userManagement/icon2.png';
@@ -7,12 +7,53 @@ import lock from '../assets/userManagement/lock.png';
 import email from '../assets/userManagement/email.png';
 import google from '../assets/userManagement/google.png';
 import user from '../assets/userManagement/user.png';
-import { Button } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Spinner } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessages('Please fill in all fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessages(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) throw new Error(data.message || 'Something went wrong');
+
+      if (data.suceess === false) {
+        return setErrorMessages(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessages(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="flex h-screen">
+    <div className="flex  min-h-screen ">
       {/* Left Side - Image and Quote */}
       <div
         className="hidden md:flex flex-1 bg-cover bg-center relative"
@@ -37,7 +78,7 @@ export default function SignUp() {
         <h2 className="text-5xl font-bold text-blue-600 -mt-5">Register</h2>
         <p className="mt-2 text-gray-500">Create a new account</p>
 
-        <form className="w-full mt-6">
+        <form className="w-full mt-6" onSubmit={handleSubmit}>
           <div className="mb-4 relative">
             <label className="block text-gray-700">Full Name</label>
             <div className="relative">
@@ -50,6 +91,8 @@ export default function SignUp() {
                 type="text"
                 className="w-full pl-10 px-4 py-2 mt-1 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="John Doe"
+                id="username"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -66,6 +109,8 @@ export default function SignUp() {
                 type="email"
                 className="w-full pl-10 px-4 py-2 mt-1 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="example@email.com"
+                id="email"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -82,13 +127,32 @@ export default function SignUp() {
                 type="password"
                 className="w-full pl-10 px-4 py-2 mt-1 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="********"
+                id="password"
+                onChange={handleChange}
               />
             </div>
           </div>
 
-          <Button gradientDuoTone="purpleToBlue" className="w-full mt-4 h-11">
-            REGISTER
+          <Button
+            type="submit"
+            gradientDuoTone="purpleToBlue"
+            className="w-full mt-4 h-11"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">Loading...</span>
+              </>
+            ) : (
+              'Sign Up'
+            )}
           </Button>
+          {errorMessages && (
+            <Alert className="mt-5" color="failure">
+              {errorMessages}
+            </Alert>
+          )}
         </form>
         <div className="flex items-center w-full my-6 ">
           <hr className="flex-grow border-gray-300" />
