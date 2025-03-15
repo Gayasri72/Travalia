4b82fc7c5479d21d99cfe0a226b4dca647b1a52a
KAVINDU <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2025-03-15 17:05:14 +0530

    complete signin page functionality

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import login from '../assets/userManagement/login.png';
 import icon1 from '../assets/userManagement/icon1.png';
 import icon2 from '../assets/userManagement/icon2.png';
@@ -7,10 +7,50 @@ import lock from '../assets/userManagement/lock.png';
 import email from '../assets/userManagement/email.png';
 import google from '../assets/userManagement/google.png';
 import fingerprintIcon from '../assets/userManagement/fingerprint.png';
-import { Button } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Button, Spinner, Alert } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessages('Please fill in all fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessages(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) throw new Error(data.message || 'Something went wrong');
+
+      if (data.success === false) {
+        return setErrorMessages(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessages(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex h-screen">
       {/* Left Side - Image and Quote */}
@@ -37,7 +77,7 @@ export default function SignIn() {
         <h2 className="text-5xl font-bold text-blue-600 -mt-20">Welcome</h2>
         <p className="mt-2 text-gray-500">Login with Email</p>
 
-        <form className="w-full mt-6">
+        <form className="w-full mt-6" onSubmit={handleSubmit}>
           <div className="mb-4 relative">
             <label className="block text-gray-700">Email</label>
             <div className="relative">
@@ -47,9 +87,11 @@ export default function SignIn() {
                 className="absolute left-3 top-3 w-5 h-5"
               />
               <input
+                id="email"
                 type="email"
                 className="w-full pl-10 px-4 py-2 mt-1 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="example@email.com"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -63,21 +105,42 @@ export default function SignIn() {
                 className="absolute left-3 top-3 w-5 h-5"
               />
               <input
+                id="password"
                 type="password"
                 className="w-full pl-10 px-4 py-2 mt-1 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder="********"
+                onChange={handleChange}
               />
             </div>
           </div>
 
-          <div className="text-right text-blue-600 cursor-pointer">
+          <Link to="/forgot-password" className="text-right text-blue-600">
             Forgot your password?
-          </div>
+          </Link>
 
-          <Button gradientDuoTone="purpleToBlue" className="w-full mt-4 h-11">
-            LOGIN
+          <Button
+            type="submit"
+            gradientDuoTone="purpleToBlue"
+            className="w-full mt-4 h-11"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">Loading...</span>
+              </>
+            ) : (
+              'Sign In'
+            )}
           </Button>
+
+          {errorMessages && (
+            <Alert className="mt-5" color="failure">
+              {errorMessages}
+            </Alert>
+          )}
         </form>
+
         <div className="flex items-center w-full my-6 ">
           <hr className="flex-grow border-gray-300" />
           <span className="mx-2 text-gray-500">OR</span>
