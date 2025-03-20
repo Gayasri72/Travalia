8568c46ea34a82f3873f08684453c75de187ccc4
Janindu <janindujayasundara@gmail.com>
2025-03-20 16:37:03 +0530

    develop api features.filtering,sorting pagination and specila routes

--- Backend/controllers/tour.controller.js
+++ Backend/controllers/tour.controller.js
@@ -1,8 +1,58 @@
 import Tour from '../models/tour.model.js';
 
+export const aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 export const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //build query
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) feild limiting
+
+    if (req.body.fields) {
+      const fields = req.query.fields.split(',').join('');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocumnets();
+      if (skip >= numTours) throw new Error('This page not exist');
+    }
+
+    //execute query
+    const tours = await query;
+    //send response
     res.status(200).json({
       success: true,
       results: tours.length,
@@ -86,4 +136,3 @@ export const deleteTour = async (req, res) => {
     });
   }
 };
-
