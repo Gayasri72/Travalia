e6becdc8ed6a629c7b537ec9b30f8d4128dea708
Gayasri72 <gayasripethum1@gmail.com>
2025-04-09 10:57:11 +0530

    update the travel itinery function

--- Backend/routes/itinerary.route.js
+++ Backend/routes/itinerary.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { createItinerary } from '../controllers/itinerary.controller.js';
+import {generateItinerary} from '../controllers/itinerary.controller.js';
 
 const router = express.Router();
 
-router.post('/generate', createItinerary);
+router.post('/generate', generateItinerary);
 
 export default router;
